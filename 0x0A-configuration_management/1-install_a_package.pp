ad0be0ef1e738289f5b698f4badf22ee757db2bc
123harri <harrietwanjiku90@gmail.com>
2024-03-22 03:59:04 -0700

    a

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,7 @@
+#!/usr/bin/pup
 # Description: This Puppet manifest installs Flask version 2.1.0 using pip3.
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'],  # Add additional paths if needed
-  environment => [],
-  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Description: This Puppet manifest kills a process named "killmenow" using pkill.
 
 exec { 'kill_killmenow_process':
-  command     => 'pkill killmenow',
-  path        => ['/bin', '/usr/bin'],  # Add additional paths if needed
-  onlyif      => 'pgrep killmenow',
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
